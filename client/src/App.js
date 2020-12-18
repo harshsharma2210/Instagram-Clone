8de60809edc728d6a8a0765978df6b4e209132ed
Harsh <harshsharma102000@gmail.com>
2020-12-18 20:04:29 +0530

    Final Website

--- client/src/App.js
+++ client/src/App.js
@@ -1,68 +1,74 @@
-import React, { useEffect, createContext, useReducer,useContext } from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
-import Home from './components/screens/Home';
-import Profile from './components/screens/Profile';
-import Signup from './components/screens/Signup';
-import Signin from './components/screens/Signin';
-import CreatePost from './components/screens/CreatePost';
-import { reducer, initialState } from './reducers/userReducer';
-import UserProfile from './components/screens/UserProfile';
-import SubscribedUserPosts from './components/screens/SubscribeUserPost';
+import React,{useEffect,createContext,useReducer,useContext} from 'react';
+import NavBar from './components/Navbar'
+import "./App.css"
+import {BrowserRouter,Route,Switch,useHistory} from 'react-router-dom'
+import Home from './components/screens/Home'
+import Signin from './components/screens/SignIn'
+import Profile from './components/screens/Profile'
+import Signup from './components/screens/Signup'
+import CreatePost from './components/screens/CreatePost'
+import {reducer,initialState} from './reducers/userReducer'
+import UserProfile from './components/screens/UserProfile'
+import SubscribedUserPosts from './components/screens/SubscribesUserPosts'
+import Reset from './components/screens/Reset'
+import NewPassword from './components/screens/Newpassword'
+export const UserContext = createContext()
 
 
-export const UserContext = createContext();
-
-const Routing = () => {
-  const history = useHistory();
-  // eslint-disable-next-line no-unused-vars
-  const {state,dispatch}=useContext(UserContext);
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem("user"));
-    if(user)
-    {
+const Routing = ()=>{
+  const history = useHistory()
+  const {state,dispatch} = useContext(UserContext)
+  useEffect(()=>{
+    const user = JSON.parse(localStorage.getItem("user"))
+    if(user){
       dispatch({type:"USER",payload:user})
+    }else{
+      if(!history.location.pathname.startsWith('/reset'))
+           history.push('/signin')
     }
-    else{
-      history.push('/signin');
-    }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return (
+  },[])
+  return(
     <Switch>
-      <Route exact path='/'>
-        <Home />
+      <Route exact path="/" >
+      <Home />
       </Route>
-      <Route path='/signin'>
+      <Route path="/signin">
         <Signin />
       </Route>
-      <Route path='/signup'>
+      <Route path="/signup">
         <Signup />
       </Route>
-      <Route exact path='/profile'>
+      <Route exact path="/profile">
         <Profile />
       </Route>
-      <Route path='/profile/:userid'>
+      <Route path="/create">
+        <CreatePost/>
+      </Route>
+      <Route path="/profile/:userid">
         <UserProfile />
       </Route>
-      <Route path='/create'>
-        <CreatePost />
-      </Route>
-      <Route path='/myfollowingpost'>
+      <Route path="/myfollowingpost">
         <SubscribedUserPosts />
       </Route>
+      <Route exact path="/reset">
+        <Reset/>
+      </Route>
+      <Route path="/reset/:token">
+        <NewPassword />
+      </Route>
+      
     </Switch>
-
   )
 }
+
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state,dispatch] = useReducer(reducer,initialState)
   return (
-    <UserContext.Provider value={{ state, dispatch }}>
-      <BrowserRouter>
-        <Navbar />
-        <Routing />/
+    <UserContext.Provider value={{state,dispatch}}>
+    <BrowserRouter>
+      <NavBar />
+      <Routing />
+      
     </BrowserRouter>
     </UserContext.Provider>
   );
